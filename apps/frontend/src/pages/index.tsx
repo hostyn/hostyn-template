32f31a302c6d06a99c170c229d9f8c393941da17
Hostyn <ruben@martinezhostyn.com>
2023-06-07 21:19:48 +0200

    feat(frontend): :sparkles: Improve auth example

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { trpc } from "@services/trpc";
 
 export default function Home() {
-  const login = trpc.auth.login.useMutation();
-  const logout = trpc.auth.login.useMutation();
+  const trpcContext = trpc.useContext();
+  const login = trpc.auth.login.useMutation({
+    onSuccess: () => trpcContext.auth.invalidate(),
+  });
+  const logout = trpc.auth.logout.useMutation({
+    onSuccess: () => trpcContext.auth.invalidate(),
+  });
+  const { data: session } = trpc.auth.getSession.useQuery();
 
   const handleLogin = () => {
     login.mutate();
@@ -17,6 +23,7 @@ export default function Home() {
       <h1>Home</h1>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
+      <h1>{JSON.stringify(session)}</h1>
     </>
   );
 }
