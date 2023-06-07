44bc5c1b5592975b8588ff8ff8068209cfd9d060
Hostyn <ruben@martinezhostyn.com>
2023-06-07 19:24:56 +0200

    feat(frontend): :sparkles: Add login and logout buttons to index.tsx

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,11 +1,22 @@
 import { trpc } from "@services/trpc";
 
 export default function Home() {
-  const { data } = trpc.hello.useQuery();
+  const login = trpc.auth.login.useMutation();
+  const logout = trpc.auth.login.useMutation();
+
+  const handleLogin = () => {
+    login.mutate();
+  };
+
+  const handleLogout = () => {
+    logout.mutate();
+  };
+
   return (
     <>
       <h1>Home</h1>
-      <button onClick={() => console.log(data)}>click</button>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
     </>
   );
 }
