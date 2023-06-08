a283af5d6fdab68e5c418132a437403520531464
Hostyn <ruben@martinezhostyn.com>
2023-06-08 09:47:18 +0200

    fix(repo): :rotating_light: Remove linter error

--- apps/frontend/src/services/trpc.ts
+++ apps/frontend/src/services/trpc.ts
@@ -6,6 +6,7 @@ import type { AppRouter } from "backend";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:3000`; // dev SSR should use localhost
