f22603baf80917cee5cb3d851bf3c39ba475d9ca
Hostyn <ruben@martinezhostyn.com>
2023-06-09 19:36:43 +0200

    feat(frontend): :sparkles: Add css globals and font

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import { trpc } from "@services/trpc";
+import "@styles/globals.css";
+import { createGlobalStyle } from "styled-components";
+import colors from "ui/config/theme";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"], variable: "--font-nunito" });
+
+const GlobalStyle = createGlobalStyle`
+    html {
+      background-color: ${colors.grey[900]};
+      color: ${colors.primary[100]};
+    }
+  `;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={nunito.className}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default trpc.withTRPC(MyApp);
